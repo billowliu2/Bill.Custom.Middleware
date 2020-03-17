8a725eb79956c63c8468502c93f2a895b9718005
billowliu2 <billowliu2016@outlook.com>
2020-03-17 12:31:57 +0800

    个人练习

---
+++ Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Bill.Custom.Middleware.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bill.Custom.Middleware.Extension
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandle(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandleMiddleware>();
+
+            return app;
+        }
+
+    }
+}
---
+++ Middleware/ExceptionHandleMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Bill.Custom.Middleware.Result;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Bill.Custom.Middleware.Middleware
+{
+    public  class ExceptionHandleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+
+        public  ExceptionHandleMiddleware( RequestDelegate next,  ILogger<ExceptionHandleMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var error =  exception.ToString();
+
+            _logger.LogError(error);
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(ResultModel.Failed(error), new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            }));
+        }
+
+    }
+}
